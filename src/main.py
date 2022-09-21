1b13656b00d319a521206f67b0d78e6ec2b16413
Reaver113 <jasonalcock113@gmail.com>
2022-09-21 16:38:40 +1000

    app init complete

--- src/main.py
+++ src/main.py
@@ -1,8 +1,11 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 def create_app():
     #Create flask app object
     app = Flask(__name__)
     app.config.from_object("config.app_config")
+
+    db = SQLAlchemy(app)
 
     return app
