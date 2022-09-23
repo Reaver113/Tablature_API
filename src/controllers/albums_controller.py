e0917416030f375723ed53c218f57be6305e5e0f
Reaver113 <jasonalcock113@gmail.com>
2022-09-23 16:29:00 +1000

    put and delete methods added

--- src/controllers/albums_controller.py
+++ src/controllers/albums_controller.py
@@ -4,22 +4,24 @@ from models.albums import Album
 from schemas.album_schema import album_schema, albums_schema
 
 albums = Blueprint("albums", __name__, url_prefix="/albums")
-
+# Query all albums
 @albums.route("/", methods =["GET"])
 def get_albums():
-    #Query all albums
     albums_list = Album.query.all()
     result = albums_schema.dump(albums_list)
     return jsonify(result)
 
-
+# Query an album by id
 @albums.route("/<int:id>", methods =["GET"])
 def get_album(id):
-    #Query an album by id
     album = Album.query.get(id)
+    if not album:
+        return{"Error": "Album not found"}
     result = album_schema.dump(album)
     return jsonify(result)
 
+
+# Post an album
 @albums.route("/", methods=["POST"])
 def new_album():
     album_fields = album_schema.load(request.json)
@@ -28,5 +30,28 @@ def new_album():
         release = album_fields["release"]
     )
     db.session.add(album)
+    db.session.commit()
+    return jsonify(album_schema.dump(album))
+
+# Delete an album
+@albums.route("/<int:id>", methods = ["DELETE"])
+def delete_album(id):
+    album = Album.query.get(id)
+    if not album:
+        return{"Error": "Album not found"}
+    db.session.delete(album)
+    db.session.commit()
+    return {"Message": "Album removed successfully"}
+
+
+# Update an album
+@albums.route("/<int:id>", methods = ["PUT"])
+def update_album(id):
+    album = Album.query.get(id)
+    if not album:
+        return{"Error": "Album not found"}
+    album_fields = album_schema.load(request.json)
+    album.name = album_fields["name"]
+    album.genre = album_fields["release"]
     db.session.commit()
     return jsonify(album_schema.dump(album))
--- src/controllers/artists_controller.py
+++ src/controllers/artists_controller.py
@@ -5,31 +5,55 @@ from schemas.artist_schema import artist_schema, artists_schema
 
 artists = Blueprint("artists", __name__, url_prefix="/artists")
 
+# Query all artists
 @artists.route("/", methods =["GET"])
 def get_artists():
-    #Query all artists
     artists_list = Artist.query.all()
     result = artists_schema.dump(artists_list)
     return jsonify(result)
 
 
-
+# Query an Artist by id
 @artists.route("/<int:id>", methods =["GET"])
 def get_artist(id):
-    #Query an artist by id
     artist = Artist.query.get(id)
+    if not artist:
+        return{"Error": "Artist not found"}
     result = artist_schema.dump(artist)
     return jsonify(result)
 
-
+# Post and Artist
 @artists.route("/", methods=["POST"])
 def new_artist():
     artist_fields = artist_schema.load(request.json)
     artist = Artist(
         name = artist_fields["name"],
         genre = artist_fields["genre"],
-        # ALBUMS TO BE ADDED
+        # ALBUMS TO BE ADDED WITH FORIEGN KEY
     )
     db.session.add(artist)
+    db.session.commit()
+    return jsonify(artist_schema.dump(artist))
+
+# Delete an Artist
+@artists.route("/<int:id>", methods = ["DELETE"])
+def delete_artist(id):
+    artist = Artist.query.get(id)
+    if not artist:
+        return{"Error": "Artist not found"}
+    db.session.delete(artist)
+    db.session.commit()
+    return {"Message": "Artist deleted successfully"}
+
+
+# Update an artist
+@artists.route("/<int:id>", methods = ["PUT"])
+def update_artist(id):
+    artist = Artist.query.get(id)
+    if not artist:
+        return{"Error": "Artist not found"}
+    artist_fields = artist_schema.load(request.json)
+    artist.name = artist_fields["name"]
+    artist.genre = artist_fields["genre"]
     db.session.commit()
     return jsonify(artist_schema.dump(artist))
