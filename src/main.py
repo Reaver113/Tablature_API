b218482be4350a20a5cdacf80c8a5be9abba2240
Reaver113 <jasonalcock113@gmail.com>
2022-09-23 17:02:49 +1000

    typo fixed

--- src/main.py
+++ src/main.py
@@ -1,16 +1,29 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager
+
+# Creatting objcts
 
 db = SQLAlchemy()
 ma = Marshmallow()
+bcrypt = Bcrypt()
+jwt = JWTManager()
+
 
 def create_app():
-    #Create flask app object
+    # Create flask app object
     app = Flask(__name__)
     app.config.from_object("config.app_config")
+
+    # init instances
     db.init_app(app)
     ma.init_app(app)
+    bcrypt.init_app(app)
+    jwt.init_app(app)
+
+
     from commands import db_commands
     app.register_blueprint(db_commands)
     from controllers import registerable_controllers
